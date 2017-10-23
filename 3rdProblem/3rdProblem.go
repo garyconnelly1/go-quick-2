0a2a58c5878f984777e3d663785f3bb27e999883
GARY CONNELLY - Student <G00336837@gmit.ie>
2017-10-23 19:15:58 +0100

    Problem 4 part 2 message struct

--- 3rdProblem/3rdProblem.go
+++ 3rdProblem/3rdProblem.go
@@ -8,20 +8,37 @@ package main
 import (
 	
 	"net/http"
+	"html/template"
 	
 )
+
+type message struct {
+	MyMessage string
+	
+}
+
+
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 
 	http.ServeFile(w, r, "index.html")
 
+	
+
 }
 
 
 func guessHandler(w http.ResponseWriter, r *http.Request){
+	msm :="Guess a number between 1 and 20"
+	
+
 	//http.ServeFile(w, r, "3rdProblem/index.html")
 	http.ServeFile(w, r, "guess.html")
+
+	t, _ := template.ParseFiles("guess.tmpl")
+
+	t.Execute(w, &message{MyMessage:msm})
 }
 
 
