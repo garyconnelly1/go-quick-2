295880c2182f51e7a00d7184f1f6c76e501026d8
GARY CONNELLY - Student <G00336837@gmit.ie>
2017-10-26 19:19:04 +0100

    more problem 6

--- 3rdProblem/3rdProblem.go
+++ 3rdProblem/3rdProblem.go
@@ -4,17 +4,19 @@
 //"fmt"
 //"math/rand"
 
+/*
+"time"
+	"strconv"
+	"html/template"
+	"math/rand"
+	*/
+
 package main
 
 import (
 	
 	"net/http"
 	"html/template"
-	"math/rand"
-	"time"
-	"strconv"
-	
-	
 	//rand.Intn(20-1) -- to generate a number between 1 and 20
 	
 )
@@ -25,68 +27,22 @@ type message struct {
 }
 
 
-
-func requestHandler(w http.ResponseWriter, r *http.Request) {
+func requestHandler(w http.ResponseWriter, r *http.Request) {	
 	w.Header().Set("Content-Type", "text/html")
-
 	http.ServeFile(w, r, "index.html")
-
-	
 
 }
 
 
 func guessHandler(w http.ResponseWriter, r *http.Request){
-	msm :="Guess a number between 1 and 20"
 
+	msm :=&message{MyMessage: "Guess a number between 1 and 20"}
+
+	 t, _ := template.ParseFiles("guess.tmpl") 
+
+	t.Execute(w, msm)
 	
-
-	target:= rand.Intn(20-1)
-
-	var ck, err = r.Cookie("target")
-
-//dont fully understand how this statement with error handling works.
-	if err == nil{
-		//if we could read it ,try to convert its value to an int
-		target, _ = strconv.Atoi(ck.Value)
-		
-	}
-
-    guess1,_ := strconv.Atoi(r.FormValue("Guess"))
-
-	if guess1== target{
-		msm ="Yess"
-	}else if guess1 < target{
-	   msm="Too low: Try again"
-	}else {
-		msm="Too high: Try again"
-	 }
-
-
-//adapted from https://stackoverflow.com/questions/12130582/setting-cookies-in-golang-net-http
-	 expires := time.Now().AddDate(1, 0, 0) // cookie will expire after 1 year
-
-    ck = &http.Cookie{
-        Name: "target",
-        Value: strconv.Itoa(target),
-        //Path: "/",
-        Expires: expires,
-    }
-
-	//set the cookie
-	http.SetCookie(w,ck)
-	
-
-	//http.ServeFile(w, r, "3rdProblem/index.html")
-	http.ServeFile(w, r, "guess.html")
-
-	t, _ := template.ParseFiles("guess.tmpl")
-
-	t.Execute(w, &message{MyMessage:msm})
 }//end guesshandler
-
-
-
 
 
 //main method
