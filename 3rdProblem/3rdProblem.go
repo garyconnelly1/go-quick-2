67c8f8124f01c27d4c5d76936959a6aa7e725ef1
GARY CONNELLY - Student <G00336837@gmit.ie>
2017-10-26 19:38:23 +0100

    trying to set up the cookies properly

--- 3rdProblem/3rdProblem.go
+++ 3rdProblem/3rdProblem.go
@@ -17,6 +17,9 @@ import (
 	
 	"net/http"
 	"html/template"
+	"math/rand"
+	"strconv"
+	"time"
 	//rand.Intn(20-1) -- to generate a number between 1 and 20
 	
 )
@@ -37,6 +40,37 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 func guessHandler(w http.ResponseWriter, r *http.Request){
 
 	msm :=&message{MyMessage: "Guess a number between 1 and 20"}
+
+	target:=rand.Intn(20-1)
+
+	var ck, err = r.Cookie("target")//gets cookie called count
+
+	//guess1,_ := strconv.Atoi(r.FormValue("Guess"))
+
+	if err == nil{
+		//if we could read it ,try to convert its value to an int
+		target, _ = strconv.Atoi(ck.Value)
+		
+		if target ==0{
+			target = rand.Intn(20-1)
+		}//end if
+		
+	}//end if
+
+	expires := time.Now().AddDate(1, 0, 0) // cookie will expire after 1 year
+
+    ck = &http.Cookie{
+		Name: "target",
+        Value: strconv.Itoa(target),
+        Path: "/",
+        Expires: expires,
+    }
+
+	//set the cookie
+	http.SetCookie(w,ck)
+	
+
+
 
 	 t, _ := template.ParseFiles("guess.tmpl") 
 
